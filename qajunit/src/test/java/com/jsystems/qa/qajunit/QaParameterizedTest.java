7ecab0352e53ff7b1062b52f84a29ecf5e2b2ff9
lrybka <lukaszrybka1983@gmail.com>
2019-11-19 10:44:24 +0100

    @Before,After...

--- qajunit/src/test/java/com/jsystems/qa/qajunit/QaParameterizedTest.java
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/QaParameterizedTest.java
@@ -1,4 +1,44 @@
 package com.jsystems.qa.qajunit;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.Assert.assertTrue;
+
+
 public class QaParameterizedTest {
+
+
+    @ParameterizedTest(name = "Parameter test with value {0}")
+    @ValueSource(ints = {5, 15, 25})
+    public void firstParameterizedTest(int number) {
+
+
+        assertTrue(number % 5 == 0);
+
+//        int[] ints = {5, 15, 25};
+//        for (int i=0; i<ints.length; i++){
+//        }
+
+    }
+
+
+    @ParameterizedTest(name = "Parameter test with value {0}")
+    @ValueSource(strings = {"Hello", "Hello junit", "Hello students"})
+    public void secondParameterizedTest(String text) {
+
+        assertTrue(text.contains("Hello"));
+
+    }
+
+    @ParameterizedTest(name = "Parameter test with value {0} and {1}")
+    @CsvSource(value = {"Hello, 5", "Hello junit 5, 15", "'Hello students 5!', 25"}, delimiter = ',')
+    public void nextParameterizedTest(String text, int number) {
+
+        assertTrue(text.contains("Hello"));
+        assertTrue(number % 5 == 0);
+
+    }
+
 }
