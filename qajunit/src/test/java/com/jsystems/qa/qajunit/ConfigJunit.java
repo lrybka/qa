9e5d4562f3614760edf8fcfcf883db3294e13658
lrybka <lukaszrybka1983@gmail.com>
2019-11-19 09:32:23 +0100

    @Before,After...

--- qajunit/src/test/java/com/jsystems/qa/qajunit/ConfigJunit.java
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/ConfigJunit.java
@@ -1,17 +1,17 @@
 package com.jsystems.qa.qajunit;
 
-import org.junit.Before;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public class ConfigJunit {
     @BeforeAll
-    public static void setupAll(){
-        System.out.println("==========setupAll===========");
+    public static void setupAll() {
+        System.out.println("===================BeforeAll===========================");
     }
 
     @AfterAll
-    public static void tearDownAll(){
-        System.out.println("==========AfetrAll===========");
+    public static void tearDownAll() {
+        System.out.println("===================AfterAll===========================");
     }
+
 }
