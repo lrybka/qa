9aa35295976c26f0e3b34acabaff6bf8419a079d
lrybka <lukaszrybka1983@gmail.com>
2019-11-19 12:00:36 +0100

    Zadanko 1

--- qajunit/src/test/java/com/jsystems/qa/qajunit/JunitTest.java
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/JunitTest.java
@@ -81,6 +81,26 @@ public class JunitTest extends ConfigJunit{
     }
 
 
+    @Test
+    public void zad1(){
+
+        String resultString = "Wordpress powers 34% of the internet";
+        String expectedString = "Wordpress powers [number]% of the internet";
+
+            assertTrue(resultString.startsWith("Wordpress powers "));
+            assertTrue(resultString.endsWith("% of the internet"));
+            assertThat(resultString).matches("(Wordpress powers )\\d+(% of the internet)");
+
+         String result = resultString.replace("Wordpress powers ", "").replace("% of the internet", "");
+         int resultNumber = Integer.parseInt(result);
+         assertTrue(resultNumber > 0);
+
+    }
+
+
+
+
+
     @Nested
     public class NestedTest {
 
