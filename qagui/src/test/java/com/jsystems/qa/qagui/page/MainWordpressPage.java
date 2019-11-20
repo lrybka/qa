e7000e51e6571fea5522cca9d314143a9788e5d8
lrybka <lukaszrybka1983@gmail.com>
2019-11-20 12:29:32 +0100

    Wordpress login automation - moje dane, drivers v2, komentarze, cd 2, poprawka w pom.xml v3, zmiana usera wordpress z mail na user, nowe dependency, wyrownanie dependecy we wszystkich pom'ach.xml, plik config.conf cd, no, nowe klasy

---
+++ qagui/src/test/java/com/jsystems/qa/qagui/page/MainWordpressPage.java
@@ -0,0 +1,14 @@
+package com.jsystems.qa.qagui.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class MainWordpressPage extends  BasePage{
+
+    public MainWordpressPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+
+
+}
