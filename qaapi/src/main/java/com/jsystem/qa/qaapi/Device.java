886689b07a6221e8d7995a71cd98b84d952bfb00
lrybka <lukaszrybka1983@gmail.com>
2019-11-21 15:37:58 +0100

    API TEST

--- qaapi/src/main/java/com/jsystem/qa/qaapi/Device.java
+++ qaapi/src/main/java/com/jsystem/qa/qaapi/Device.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class Device {
+
+    @JsonProperty(required = true)
     public String type;
 
     @JsonProperty(value = "device.model", required = true)
--- qaapi/src/main/java/com/jsystem/qa/qaapi/User.java
+++ qaapi/src/main/java/com/jsystem/qa/qaapi/User.java
@@ -1,8 +1,18 @@
 package com.jsystem.qa.qaapi;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class User {
+
+    @JsonProperty(required = true)
     public String imie;
+
+    @JsonProperty(required = true)
     public String nazwisko;
 
+    @JsonProperty(required = true)
+    public List<Device> device;
 
 }
