92b3da14b16e6fcce39455f339cfea9db69519d8
lrybka <lukaszrybka1983@gmail.com>
2019-11-21 08:18:33 +0100

    refaktoryzacja

--- qagui/src/test/java/com/jsystems/qa/qagui/page/BasePage.java
+++ qagui/src/test/java/com/jsystems/qa/qagui/page/BasePage.java
@@ -1,14 +1,31 @@
 package com.jsystems.qa.qagui.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
     public WebDriver driver;
+    private WebDriverWait wait  = new WebDriverWait(driver, 30);
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
     }
 
-
+    public void waitForElementToBeClicable(By by, int time) {
+        wait = new WebDriverWait(driver, time);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+    public void waitForElementToBeClicable(By by) {
+        waitForElementToBeClicable(by, 30);
+    }
+    public void waitForElementToBeVisibility(By by, int time) {
+        wait = new WebDriverWait(driver, time);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+    public void waitForElementToBeVisibility(By by) {
+        waitForElementToBeVisibility(by, 30);
+    }
 
 }
