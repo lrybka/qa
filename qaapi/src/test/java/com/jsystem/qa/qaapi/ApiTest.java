f72aeb73169d61af3ab5627346fa34a4205b7044
lrybka <lukaszrybka1983@gmail.com>
2019-11-21 14:42:21 +0100

    API TEST

---
+++ qaapi/src/test/java/com/jsystem/qa/qaapi/ApiTest.java
@@ -0,0 +1,14 @@
+package com.jsystem.qa.qaapi;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("ApiTest")
+public class ApiTest {
+
+    @Test
+    public void firstApiTest(){
+
+    }
+
+}
