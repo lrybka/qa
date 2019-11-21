17ac91bfb563aa26683e1167fbbe0087c0e26c77
lrybka <lukaszrybka1983@gmail.com>
2019-11-21 10:49:35 +0100

    nowe pakiety

--- qagui/src/test/java/com/jsystems/qa/qagui/cucumber/ConfigStepCucumber.java
+++ qagui/src/test/java/com/jsystems/qa/qagui/cucumber/ConfigStepCucumber.java
@@ -1,4 +1,28 @@
 package com.jsystems.qa.qagui.cucumber;
 
+import org.openqa.selenium.WebDriver;
+
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
 public class ConfigStepCucumber {
+
+    protected WebDriver driver;
+
+    //    String chromePath;
+    String fireFoxPath;
+    {
+        try {
+//            chromePath = Paths.get(getClass().getClassLoader().getResource("drivers/chromedriver.exe")
+//                    .toURI()).toFile().getAbsolutePath();
+            fireFoxPath = Paths.get(getClass().getClassLoader().getResource("drivers/geckodriver.exe")
+                    .toURI()).toFile().getAbsolutePath();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
