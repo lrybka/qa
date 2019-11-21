d6001aba2e46a0be9bcb16c985ff1bd658d4c958
lrybka <lukaszrybka1983@gmail.com>
2019-11-21 14:59:34 +0100

    API TEST

--- qaapi/src/test/java/com/jsystem/qa/qaapi/ApiTest.java
+++ qaapi/src/test/java/com/jsystem/qa/qaapi/ApiTest.java
@@ -1,14 +1,24 @@
 package com.jsystem.qa.qaapi;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
 
 @Tag("ApiTest")
 public class ApiTest {
 
     @Test
     public void firstApiTest(){
-
+        RestAssured
+                .given()
+                .get("http://www.mocky.io/v2/5a6b69ec3100009d211b8aeb")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("name",equalTo("Piotr"))
+                .body("surname",equalTo("Kowalski"));
     }
 
 }
