b460891cf48fcc16ea8244a9871d4339b63016ab
lrybka <lukaszrybka1983@gmail.com>
2019-11-21 15:36:26 +0100

    API TEST

--- qaapi/src/main/java/com/jsystem/qa/qaapi/DeviceModel.java
+++ qaapi/src/main/java/com/jsystem/qa/qaapi/DeviceModel.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class DeviceModel {
 
+    @JsonProperty(required = true)
     public String produce;
     @JsonProperty(value = "screen.size", required = true)
     public double screenSize;
