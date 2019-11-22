98d78fb322d32f0a7c1a38c3adb3b797943f2cbf
lrybka <lukaszrybka1983@gmail.com>
2019-11-22 10:27:26 +0100

    refaktor ALL 2, pobieranie danych z bazy danych

--- qaapi/src/main/java/com/jsystems/qa/qaapi/service/azure/BookService.java
+++ qaapi/src/main/java/com/jsystems/qa/qaapi/service/azure/BookService.java
@@ -1,7 +1,22 @@
 package com.jsystems.qa.qaapi.service.azure;
 
+import com.jsystems.qa.qaapi.model.azure.book.Book;
+import com.jsystems.qa.qaapi.specification.Specification;
+import io.restassured.RestAssured;
+
 public class BookService {
 
+    public static void postBook(Book book, int statusCode) {
+        RestAssured.given()
+                .spec(Specification.fakeAzureSpecBuilder())
+                .when()
+                .body(book)
+                .post("api/Books")
+                .then()
+                .assertThat()
+                .statusCode(statusCode)
 
+                ;
+    }
 
 }
