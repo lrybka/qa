c8ca0132dac70d28c682b0244edd269b64ff7723
lrybka <lukaszrybka1983@gmail.com>
2019-11-22 09:30:18 +0100

    refaktor ALL 2, pobieranie danych z bazy danych

--- qaapi/src/main/java/com/jsystems/qa/qaapi/model/azure/AzureAuthor.java
+++ qaapi/src/main/java/com/jsystems/qa/qaapi/model/azure/AzureAuthor.java
@@ -1,5 +1,12 @@
 package com.jsystems.qa.qaapi.model.azure;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class AzureAuthor {
+
+    @JsonProperty(value = "device.model", required = true)
+    public List<AzureAuthor> deviceModel;
 
 }
