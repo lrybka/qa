0ec0f69f4c3ad4b3212b170e9b81680509aabc67
lrybka <lukaszrybka1983@gmail.com>
2019-11-22 10:19:46 +0100

    refaktor ALL 2, pobieranie danych z bazy danych

--- qaapi/src/main/java/com/jsystems/qa/qaapi/model/azure/book/Book.java
+++ qaapi/src/main/java/com/jsystems/qa/qaapi/model/azure/book/Book.java
@@ -1,4 +1,27 @@
 package com.jsystems.qa.qaapi.model.azure.book;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
 public class Book {
+
+    @JsonProperty(value = "ID", required = true)
+    public long id;
+
+    @JsonProperty(value = "Title", required = true)
+    public String title;
+
+    @JsonProperty(value = "Description", required = true)
+    public String description;
+
+    @JsonProperty(value = "PageCount", required = true)
+    public int pageCount;
+
+    @JsonProperty(value = "Excerpt", required = true)
+    public String excerpt;
+
+    @JsonProperty(value = "PublishDate", required = true)
+    public String publishDate;
+
 }
